64ea67c1e5cb4a6bfab7a8d1a8dcfdf4a1d01bd8
ravjot <ravjotkaamra@gmail.com>
2020-06-16 19:47:04 +0530

    'Load Data from 'https://www.tdcj.texas.gov/death_row/dr_executed_offenders.html'

---
+++ load.py
@@ -0,0 +1,48 @@
+import urllib.request, urllib.parse, urllib.error
+import ssl
+import sqlite3
+from bs4 import BeautifulSoup
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = 'https://www.tdcj.texas.gov/death_row/dr_executed_offenders.html'
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+print('Retrieving:',url)
+conn = sqlite3.connect('executed_offenders.sqlite')
+cur = conn.cursor()
+
+cur.execute('''
+            CREATE TABLE IF NOT EXISTS Offenders
+            (id INTEGER UNIQUE, first_name TEXT, last_name TEXT, age INTEGER, date TEXT, race TEXT, country TEXT)
+            ''')
+
+tags = soup('tr')
+count = 0
+for tag in tags:
+    
+    data = tag('td')
+
+    if(len(data)<1):continue
+
+    count = count + 1
+
+    last_name = data[3].text.strip()
+    first_name = data[4].text.strip()
+    age = int(data[6].text.strip())
+    date = data[7].text.strip()
+    race = data[8].text.strip()
+    country = data[9].text.strip()
+
+    cur.execute('''INSERT OR IGNORE INTO Offenders (id, first_name, last_name, age, date, race, country) VALUES (?, ?, ?, ?, ?, ?, ?)''',
+    (count ,first_name, last_name, age, date, race, country))
+    
+    if(count%100==0):conn.commit()
+
+print('No of offenders executed:',count)
+
+conn.commit()

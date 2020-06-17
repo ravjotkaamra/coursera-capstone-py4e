7ed7b66711172ebe11e680733b08412fdc3f0f23
ravjot <ravjotkaamra@gmail.com>
2020-06-17 12:49:55 +0530

    "Added Visualisation of race vs offenders count"

---
+++ gmat1.py
@@ -0,0 +1,31 @@
+from matplotlib import pyplot as plt
+
+import sqlite3
+#Loading Data
+conn = sqlite3.connect('file:executed_offenders.sqlite?mode=ro', uri=True)
+cur = conn.cursor()
+
+cur.execute('''SELECT race FROM Offenders''')
+
+race_count = dict()
+
+for row in cur:
+    race = row[0]
+    race_count[race] = race_count.get(race, 0) + 1
+
+cur.close()
+
+#Data Visualisation
+
+plt.style.use('fivethirtyeight')
+#plt.xkcd()
+
+race_x = list(race_count.keys())
+count_y = list(race_count.values())
+
+plt.xlabel('Race')
+plt.ylabel('No. of Executed Offenders')
+plt.title('Death Row Information in Texas')
+plt.bar(race_x, count_y)
+
+plt.show()
